4367e70d5ceec9aaebc6654a4eecc7d2a1d82833
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-08 09:22:49 +0530

    Create a game board

--- TicTacToeProblem/src/TicTacToe/TicTacToeProblem.java
+++ TicTacToeProblem/src/TicTacToe/TicTacToeProblem.java
@@ -1,8 +1,38 @@
 package TicTacToe;
 
 public class TicTacToeProblem {
-	
+
+	public static void displaygrid(char[][] grid) {
+
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 2; j++) {
+				System.out.print(grid[i][j] + "|");
+			}
+			System.out.print(grid[i][2]);
+			System.out.println();
+			System.out.println("------");
+		}
+
+		for (int j = 0; j < 2; j++) {
+			System.out.print(grid[2][j] + "|");
+		}
+		System.out.print(grid[2][2]);
+		System.out.println();
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Tic Tac Toe Problem");
+		System.out.println("Welcome to Tic Tac Toe Game");
+		char[][] grid = new char[3][3];
+		grid[0][0] = ' ';
+		grid[0][1] = ' ';
+		grid[0][2] = ' ';
+		grid[1][0] = ' ';
+		grid[1][1] = ' ';
+		grid[1][2] = ' ';
+		grid[2][0] = ' ';
+		grid[2][1] = ' ';
+		grid[2][2] = ' ';
+
+		displaygrid(grid);
 	}
 }
