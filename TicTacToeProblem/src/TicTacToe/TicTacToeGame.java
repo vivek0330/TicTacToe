0157229248a3f3698acc36220b94de0b50d0cdba
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-09 00:45:19 +0530

    Creating a tic tac toe board and assign empty space to each element

--- TicTacToeProblem/src/TicTacToe/TicTacToeGame.java
+++ TicTacToeProblem/src/TicTacToe/TicTacToeGame.java
@@ -2,27 +2,15 @@ package TicTacToe;
 
 public class TicTacToeGame {
 
-	public static void gameBoard(char[][] grid) {
-
-		for (int i = 0; i < 2; i++) {
-			for (int j = 0; j < 2; j++) {
-				System.out.print(grid[i][j] + "|");
-			}
-			System.out.print(grid[i][2]);
-			System.out.println();
-			System.out.println("-----");
+	public static void createEmptyBoard() {
+		char[] board = new char[10];
+		for (int a = 1; a < board.length; a++) {
+			board[a] = ' ';
 		}
-
-		for (int j = 0; j < 2; j++) {
-			System.out.print(grid[2][j] + "|");
-		}
-		System.out.print(grid[2][2]);
-		System.out.println();
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Tic Tac Toe Game");
-		char[][] grid = new char[3][3];
-		gameBoard(grid);
+		System.out.println("Welcome Tic Tac Toe game");
+		createEmptyBoard();
 	}
 }
